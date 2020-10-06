f35a81ed24ff293fe08a70d2ef2b6332e03b12f9
Amogh Joshi <amoghj8@gmail.com>
2020-10-06 19:23:16 +0530

    Insert into a Binary Search Tree

---
+++ October/6.cpp
@@ -0,0 +1,29 @@
+// Insert into a Binary Search Tree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(!root)
+            root=getNewNode(val);
+        else if(root->val<val)
+            root->right = insertIntoBST(root->right,val);
+        else if(val<root->val)
+            root->left = insertIntoBST(root->left,val);
+        return root;
+    }
+private:
+    TreeNode* getNewNode(int val) {
+        TreeNode* temp = new TreeNode(val);
+        return temp;
+    }
+};
